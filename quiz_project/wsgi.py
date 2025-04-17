2ce19cc50b5f3476a2f2495fbeb4710996d58b3c
dopeWeb <maksgen71@gmail.com>
2025-04-18 02:41:40 +0300

    Update wsgi.py

--- quiz_project/wsgi.py
+++ quiz_project/wsgi.py
@@ -1,20 +1,5 @@
-"""
-WSGI config for quiz_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
-import os, sys
+import os
 from django.core.wsgi import get_wsgi_application
 
-
-sys.path.append(r"D:\JohnBryce\ProjecBackend_Finall")
-
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quiz_project.settings")
-
-
 application = get_wsgi_application()
